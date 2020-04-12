daa0e7f001c4ff8a4e9674415b5f8800fa081db1
laloo96 <ohmycaio@gmail.com>
2020-04-12 16:54:11 -0300

    -Agrego login funcionando contra firebase
    -Agrego Routing
    -Agrego Auth service

---
+++ AlarmadeRobo/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBcC05qvfg7m6e1y_1U6UtlTXOdwbrHJe0",
+    authDomain: "alarma-de-robo-a23a7.firebaseapp.com",
+    databaseURL: "https://alarma-de-robo-a23a7.firebaseio.com",
+    projectId: "alarma-de-robo-a23a7",
+    storageBucket: "alarma-de-robo-a23a7.appspot.com",
+    messagingSenderId: "536470637407",
+    appId: "1:536470637407:web:2c72567a120774ffc644ea",
+    measurementId: "G-08GFJLJBCP"
+  }
+};
